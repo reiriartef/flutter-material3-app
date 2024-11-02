eb8a50f5de34c13b75713553bfaca1ac365b4793
Ricardo Iriarte <95953488+LordCatalyst@users.noreply.github.com>
2024-11-02 16:58:36 -0400

    rutas con nombre implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/config/router/app_router.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = "home_screen";
   const HomeScreen({super.key});
 
   @override
@@ -61,6 +62,8 @@ class _CustomListTile extends StatelessWidget {
         // Navigator.pushNamed(context, menuItem.link);
 
         context.push(menuItem.link);
+
+        // context.pushNamed(CardsScreen.name);
       },
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
