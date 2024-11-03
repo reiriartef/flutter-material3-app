7e0783a66d92a6c5cfa96158d6cd2353a7a66427
Ricardo Iriarte <95953488+LordCatalyst@users.noreply.github.com>
2024-11-03 11:49:11 -0400

    screens creadas y barril actualizado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = "buttons_screen";
