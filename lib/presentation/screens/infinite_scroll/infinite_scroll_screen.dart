5358d52c4fedcac3e366eaa9e680ea0dbe660db4
Ricardo Iriarte <lzzow29@gmail.com>
2024-11-05 09:06:29 -0400

    Scroll infinito terminado

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,11 +1,119 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   static const name = 'infinite_screen';
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        loadNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    isMounted = false;
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    if (isLoading) return;
+    isLoading = true;
+    if (!isMounted) return;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    if (!isMounted) return;
+    setState(() {});
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    if (!isMounted) return;
+    setState(() {});
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() {
+    if ((scrollController.position.pixels + 150) <=
+        scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(scrollController.position.pixels + 120,
+        duration: const Duration(microseconds: 300),
+        curve: Curves.fastOutSlowIn);
+  }
+
+  Future<void> onRefresh() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (!isMounted) return;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    addFiveImages();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        //child: const Icon(Icons.arrow_back_ios_new_outlined),
+        child: isLoading
+            ? FadeIn(child: const CircularProgressIndicator())
+            : FadeIn(child: const Icon(Icons.arrow_back_ios_new_outlined)),
+      ),
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: RefreshIndicator(
+          edgeOffset: 10,
+          strokeWidth: 2,
+          onRefresh: onRefresh,
+          child: ListView.builder(
+            controller: scrollController,
+            physics: const BouncingScrollPhysics(),
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 300,
+                  placeholder:
+                      const AssetImage("assets/images/jar-loading.gif"),
+                  image: NetworkImage(
+                      "https://picsum.photos/id/${imagesIds[index]}/500/300"));
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
